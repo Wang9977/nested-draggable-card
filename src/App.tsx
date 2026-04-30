4216a022e89666dffb63111f095ec2f6351438c6
Yuna <44893615+Wang9977@users.noreply.github.com>
2026-04-30 18:07:51 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,23 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import React, { useRef, useState } from "react";
-import { Button, ConfigProvider, Card as AntdCard } from "antd";
+import React, { useState } from "react";
+import { ConfigProvider } from "antd";
 import zhCN from "antd/locale/zh_CN";
-import Wrapper from "./module/Wrapper";
 import { CardData } from "./module/ItemTypes";
+import Playground from "./pages/Playground";
 import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   const [cards, setCards] = useState<CardData[]>([]);
-  const id = useRef(0);
-
-  const addCard = () => {
-    id.current += 1;
-    const newCard: CardData = {
-      name: `卡片名称${id.current}`,
-      id: id.current,
-      displayDef: `卡片内容${id.current}`,
-      operator: "交",
-    };
-    setCards((prev) => [...prev, newCard]);
-  };
+  const [maxLevel, setMaxLevel] = useState(3);
 
   return (
     <ConfigProvider locale={zhCN}>
       <GlobalStyles />
-      <div className="App" style={{ padding: 24 }}>
-        <div style={{ marginBottom: 16 }}>
-          <Button type="primary" onClick={addCard}>
-            添加卡片
-          </Button>
-        </div>
-        <div>
-          <DndProvider backend={HTML5Backend}>
-            <Wrapper cards={cards} setCards={setCards} isReadOnly={false} />
-          </DndProvider>
-        </div>
-      </div>
+      <Playground
+        cards={cards}
+        setCards={setCards}
+        maxLevel={maxLevel}
+        setMaxLevel={setMaxLevel}
+      />
     </ConfigProvider>
   );
 }
